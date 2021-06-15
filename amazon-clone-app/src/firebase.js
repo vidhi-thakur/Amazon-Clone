403a11682bd51d94b8a6db078c42f974de8055d5
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-15 18:34:25 +0530

    initialized react app
    initialized the project with create-react app, added material UI and firease tools

---
+++ amazon-clone-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_GpjzZG5iMiK49C0BZZzwsWNPzHzQeak",
+  authDomain: "clone-ec622.firebaseapp.com",
+  projectId: "clone-ec622",
+  storageBucket: "clone-ec622.appspot.com",
+  messagingSenderId: "684675282541",
+  appId: "1:684675282541:web:246428ac26c53820a4b164",
+  measurementId: "G-ZC0DEZW4S9"
+};
