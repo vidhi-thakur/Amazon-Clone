1462d7ef03859c3223ac6a6830c9e03b01925ab9
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-18 20:00:25 +0530

    style: Sign In page

--- amazon-clone-app/src/pages/Signin.js
+++ amazon-clone-app/src/pages/Signin.js
@@ -4,14 +4,17 @@ import "./Signin.css"
 function Signin() {
     return (
         <div className="signIn">
+            <img src="https://clone-74fb5.web.app/" alt="" />
+        <form className="signIn__form">
             <h1>Sign In</h1>
-            <strong>Email</strong>
-            <input required type="email"/>
-            <strong>Password</strong>
-            <input required type="password"/>
-            <button type="submit">Sign In</button>
+            <label for="Email">Email</label><br />
+            <input type="email" id="Email" name="Email" /><br />
+            <label for="Password">Password</label><br />
+            <input type="password" id="Password" name="Password" />
             <p>By signing-in you agree to Amazon's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
-            <button>Create your Amazon account</button>
+            <button type="submit">Create your Amazon account</button>
+        </form>
+
         </div>
     )
 }
