ca02fa72e2bf5f36e9fa4eb5e5d84aaf676ec50c
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-19 16:14:03 +0530

    style: sum of basket component

--- amazon-clone-app/src/components/Productsum.js
+++ amazon-clone-app/src/components/Productsum.js
@@ -5,12 +5,15 @@ import { useStateValue } from "../StateProvider"
 function Productsum() {
     const [{basket}, dispatch] = useStateValue();
     return (
-        <div>
+        <div className="productsum">
             {(basket.length >= 0) && (
-                    <div className="checkout__sum">
+                    <div className="productsum__price">
                         <p>{`Subtotal ( ${basket.length} items): `}<span>₹</span><strong>0</strong></p>
-                        <input type="checkbox">This order contains a gift</input>
-                        <button>Proceed to Checkout</button>
+                        <div className="productsum__input">
+                            <input type="checkbox" value="This order contains a gift" />
+                            <label>This order contains a gift</label>
+                        </div>
+                        <button className="btn--width product__button">Proceed to Checkout</button>
                     </div>
                 )}
         </div>
