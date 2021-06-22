24abd92a763932c5f76bb52b63f42a9693592091
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-22 16:52:44 +0530

    update: payment page

--- amazon-clone-app/src/pages/Payment.js
+++ amazon-clone-app/src/pages/Payment.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
 import { basketItemSum } from "../reducer"
 import CurrencyFormat from 'react-currency-format';
+import Checkoutlist from "../components/Checkoutlist";
 
 function Payment() {
 
@@ -31,33 +32,44 @@ function Payment() {
                 <img className="linkToSignIn--image" src="https://m.media-amazon.com/images/G/31/cart/empty/kettle-desaturated._CB424694257_.svg" alt="" />
                 <Link to="/Signin"><button className="payment__signInButton">Sign in to proceed</button></Link>
             </div>: <div className="payment__userSignedIn payment--flex">
-            <div className="payment__userInfo--left">
-                <div className="payment__userCredential payment--flex">
-                    <span>Deliver to</span>
-                    <h3 className="payment__userName">{name}</h3>
-                </div>
-                <div className="payment__userCredential payment--flex">
-                    <span>Delivery Email</span>
-                    <h3 className="payment__userEmail">{user.email}</h3>
-                </div>
-                <div className="payment__userCredential payment--flex">
-                    <span>Delivery Address</span>
-                    <h3 className="payment__userAddress">{address? address: <button className="payment__signInButton" onClick={enterAddress}>Update address</button>}</h3>
-                </div>
-                <div className="payment__amount payment--flex">
-                    <h3>Order Total</h3>
-                    <CurrencyFormat renderText={(value) => (
-                    <div className=""><strong>{value}</strong>
-                    </div>
-                    )} decimalScale={2} value={basketItemSum(basket)} displayType='text' prefix={'₹'} thousandSeparator={true}  />
+            <div className="payment__top">
+                <h3>Deliver to</h3>
+                <div className="payment__userInfo">
+                    {name && <p>{name}</p>}
+                    <p>{user.email}</p>
+                    <p className="payment__userAddress">{address? address: <button className="payment__btn" onClick={enterAddress}>Update address</button>}</p>
                 </div>
             </div>
-            <div className="payment__link--right payment--flex">
-                <Link to="/">
-                    <button className="payment__signInButton">Continue shopping</button>
-                </Link>
-                <button onClick={clicked} className="payment__signInButton">{!toggle?"Pay Now": "Processing"}</button>
-            </div> 
+            <div className="payment__middle">
+                <h3>Ordered Items</h3>
+                {
+                    basket?.length===0 ? (<div className="checkout__paragraph"> <strong>Your Amazon Basket is empty.</strong>
+                    <p>Your shopping cart is waiting. Give it purpose – fill it with groceries, clothing, household supplies, electronics and more. Continue shopping on the</p> 
+                    <Link className="checkout__link" to="/">
+                    Amazon homepage
+                    </Link><span>.</span>
+                    </div>) : (<div className="checkout__paragraph"> 
+                    {
+                    basket.map((item) => (<Checkoutlist id={item.id} title={item.title} price={item.price} rating={item.rating} image={item.image} />))
+                    }
+                </div>)
+                }
+            </div>
+            <div className="payment__bottom">
+                <h3>Order Total</h3>
+                <div payment__amount>
+                <CurrencyFormat renderText={(value) => (
+                <div className=""><strong>{value}</strong>
+                </div>
+                )} decimalScale={2} value={basketItemSum(basket)} displayType='text' prefix={'₹'} thousandSeparator={true}  />
+                <div className="bottom--btn">
+                    <Link to="/">
+                        <button className="payment__signInButton">Continue shopping</button>
+                    </Link>
+                    <button onClick={clicked} className="payment__signInButton">{!toggle?"Pay Now": "Processing"}</button>
+                </div>
+                </div>
+            </div>
         </div>}
         </div>
     )
