5341ce8b5d6dc677464b4c3d22d366101261a7ee
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-22 18:10:19 +0530

    style: payment page

--- amazon-clone-app/src/pages/Payment.js
+++ amazon-clone-app/src/pages/Payment.js
@@ -25,48 +25,48 @@ function Payment() {
 
     return (
         <div className="payment">
-            <div className="payment__header">
+            <div className="borderBottom">
                 <h1>Order Summary</h1>
             </div>
         {!user? <div className="payment__linkToSignIn">
                 <img className="linkToSignIn--image" src="https://m.media-amazon.com/images/G/31/cart/empty/kettle-desaturated._CB424694257_.svg" alt="" />
-                <Link to="/Signin"><button className="payment__signInButton">Sign in to proceed</button></Link>
-            </div>: <div className="payment__userSignedIn payment--flex">
-            <div className="payment__top">
-                <h3>Deliver to</h3>
-                <div className="payment__userInfo">
+                <Link to="/Signin"><button className="payment__signInButton margin">Sign in to proceed</button></Link>
+            </div>: <div className="payment__userSignedIn flex">
+            <div className="payment__top rowWidth borderBottom flex">
+                <h3 className="payment__style small_screen_width">Deliver to</h3>
+                <div className="payment__userInfo small_screen_width">
                     {name && <p>{name}</p>}
                     <p>{user.email}</p>
-                    <p className="payment__userAddress">{address? address: <button className="payment__btn" onClick={enterAddress}>Update address</button>}</p>
+                    <p className="payment__userAddress">{address? address: <button className="payment__btn payment__signInButton" onClick={enterAddress}>Update address</button>}</p>
                 </div>
             </div>
-            <div className="payment__middle">
-                <h3>Ordered Items</h3>
+            <div className="payment__middle rowWidth borderBottom flex">
+                <h3 className="payment__style small_screen_width">Ordered Items</h3>
                 {
-                    basket?.length===0 ? (<div className="checkout__paragraph"> <strong>Your Amazon Basket is empty.</strong>
+                    basket?.length===0 ? (<div className="checkout__paragraph small_screen_width"> <strong>Your Amazon Basket is empty.</strong>
                     <p>Your shopping cart is waiting. Give it purpose – fill it with groceries, clothing, household supplies, electronics and more. Continue shopping on the</p> 
                     <Link className="checkout__link" to="/">
                     Amazon homepage
                     </Link><span>.</span>
-                    </div>) : (<div className="checkout__paragraph"> 
+                    </div>) : (<div className="checkout__paragraph small_screen_width"> 
                     {
                     basket.map((item) => (<Checkoutlist id={item.id} title={item.title} price={item.price} rating={item.rating} image={item.image} />))
                     }
                 </div>)
                 }
             </div>
-            <div className="payment__bottom">
-                <h3>Order Total</h3>
-                <div payment__amount>
+            <div className="payment__bottom rowWidth borderBottom flex">
+                <h3 className="payment__style small_screen_width">Order Total</h3>
+                <div className="payment__amount small_screen_width flex">
                 <CurrencyFormat renderText={(value) => (
-                <div className=""><strong>{value}</strong>
+                <div className="amt--fontColor"><strong>{value}</strong>
                 </div>
                 )} decimalScale={2} value={basketItemSum(basket)} displayType='text' prefix={'₹'} thousandSeparator={true}  />
-                <div className="bottom--btn">
+                <div className="bottom--btn flex">
                     <Link to="/">
-                        <button className="payment__signInButton">Continue shopping</button>
+                        <button className="payment__signInButton payment__btn">Continue shopping</button>
                     </Link>
-                    <button onClick={clicked} className="payment__signInButton">{!toggle?"Pay Now": "Processing"}</button>
+                    <button onClick={clicked} className="payment__signInButton payment__btn">{!toggle?"Pay Now": "Processing"}</button>
                 </div>
                 </div>
             </div>
